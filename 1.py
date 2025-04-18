67930b40bce119eac24228a52ad15d07f3ed451b
Chao Wang <1256405274@qq.com>
2025-04-18 17:13:15 +0800

    init proj

--- 1.py
+++ 1.py
@@ -1,17 +1,2 @@
 import torch
-import torch.nn as nn
-
-# 定义输入数据
-batch_size = 16
-in_channels = 3
-input_length = 10
-x = torch.randn(batch_size, in_channels, input_length)
-
-# 定义一维卷积层
-out_channels = 6
-kernel_size = 3
-conv1d = nn.Conv1d(in_channels, out_channels, kernel_size)
-
-# 执行卷积操作
-output = conv1d(x)
-print(output.shape)  # 输出形状: (batch_size, out_channels, output_length)
+print(torch.cuda.is_available())
---
+++ commformer/scripts/test/test_smac_8m.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+env="StarCraft2"
+map="8m"
+algo="mat"
+exp="test"
+model_dir="transformer_0.pt"  # 替换为实际模型路径
+
+echo "env is ${env}, map is ${map}, algo is ${algo}, exp is ${exp}"
+CUDA_VISIBLE_DEVICES=0 python ../train/train_smac.py --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} \
+--map_name ${map} --use_eval --model_dir ${model_dir} --eval_episodes 32
