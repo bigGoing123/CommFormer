51edbf29426086875d2aa296916d0cd2bad01185
wangchao <1256405274@qq.com>
2025-06-30 21:09:10 +0800

    modify all cudas

--- commformer/scripts/train_smac/train_smac_MMM.sh
+++ commformer/scripts/train_smac/train_smac_MMM.sh
@@ -14,13 +14,13 @@ steps=2000000
 echo "env is ${env}, map is ${map}, algo is ${algo}, exp is ${exp}, max seed is ${seed_max}"
 for seed in `seq ${seed_max}`;
 do
-  CUDA_VISIBLE_DEVICES=$1 python ../train/train_smac.py --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} \
+  CUDA_VISIBLE_DEVICES=1 python ../train/train_smac.py --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} \
     --map_name ${map} --seed ${seed} --n_training_threads 16 --n_rollout_threads 32 --num_mini_batch 1 \
     --episode_length 100 --num_env_steps ${steps} --lr 5e-4 --ppo_epoch ${ppo_epochs} --clip_param ${ppo_clip} --save_interval 100000 \
     --use_value_active_masks  --prefix_name ${name} --use_bilevel --post_stable --comm_mode lf --use_wandb
 
 
-  CUDA_VISIBLE_DEVICES=$1 python ../train/train_smac.py --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} \
+  CUDA_VISIBLE_DEVICES=1 python ../train/train_smac.py --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} \
     --map_name ${map} --seed ${seed} --n_training_threads 16 --n_rollout_threads 32 --num_mini_batch 1 \
     --episode_length 100 --num_env_steps ${steps} --lr 5e-4 --ppo_epoch ${ppo_epochs} --clip_param ${ppo_clip} --save_interval 100000 \
     --use_value_active_masks  --prefix_name ${name} --use_bilevel --post_stable --comm_mode com --use_wandb
